14d4fc20f34a62a876a0a9f90514a6ade7829cb6
Ishaan3030 <86636860+Ishaan3030@users.noreply.github.com>
2021-07-09 14:57:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,72 @@
 var bg, sleep, brush, gym;
 var eat, drink, move;
+var astronaut, b;
 
 function preload(){
   bg = loadImage("images/iss.png");
-  sleep = loadAnimation("images/sleep.png");
-  brush = loadAnimation("images/brush.png");
-  gym1 = loadAnimation("images/gym1.png", "images/gym2.png");
-  gym2 = loadAnimation("images/gym11.png", "images/gym12.png");
-  eat = loadAnimation("images/eat1.png","images/eat2.png");
-  drink = loadAnimation("images/drink1.png","images/drink2.png");
-  move = loadAnimation("images/move.png","images/move1.png");
+  sleep = loadImage("images/sleep.png");
+  brush = loadImage("images/brush.png");
+  gym1 = loadImage("images/gym1.png", "images/gym2.png");
+  gym2 = loadImage("images/gym11.png", "images/gym12.png");
+  eat = loadImage("images/eat1.png","images/eat2.png");
+  drink = loadImage("images/drink1.png","images/drink2.png");
+  move = loadImage("images/move.png","images/move1.png");
 }
 
 function setup() {
-  createCanvas(800,800);
+  createCanvas(600,460);
 
-  background = createSprite(400,400);
-  background.addImage("background",bg);
-  
-  createSprite(400, 200, 50, 50);
+  b = createSprite(300,230);
+  b.addImage("background",bg);
+
+  astronaut = createSprite(300,230);
+  astronaut.addAnimation("sleeping", sleep);
+  astronaut.scale = 0.1;
 }
 
 function draw() {
-  background(255,255,255);  
+  background("lightblue");
+  
+  if(keyDown("UP_ARROW")){
+    astronaut.addImage("brushing", brush);
+    astronaut.changeImage("brushing");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+
+  if(keyDown("DOWN_ARROW")){
+    astronaut.addImage("gymming", gym1, gym2);
+    astronaut.changeImage("gymming");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+
+  if(keyDown("LEFT_ARROW")){
+    astronaut.addImage("eating", eat);
+    astronaut.changeImage("eating");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+
+  if(keyDown("RIGHT_ARROW")){
+    astronaut.addImage("bathing", bath);
+    astronaut.changeImage("bathing");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+
+  if(keyDown("m")){
+    astronaut.addImage("moving", move);
+    astronaut.changeImage("moving");
+    astronaut.y = 350;
+    astronaut.velocityX = 2;
+    astronaut.velocityY = 1;
+  }
+
+  
   drawSprites();
 }
